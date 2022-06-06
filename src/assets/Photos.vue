357b7ebef11beee4d40c5eafe19903efd86f218c
Denny Rizky Ramdani <denny@DESKTOP-LMN9F9I.localdomain>
2022-06-06 19:34:26 +0700

    Initial commit

---
+++ src/assets/Photos.vue
@@ -0,0 +1,11 @@
+<template>
+  <img src="./foto.jpg" width="200" height="200" class="foto_profil"/>
+</template>
+
+<style>
+.foto_profil {
+  border-radius: 25px;
+  border-style: dotted;
+  border-color: hsla(160, 100%, 37%, 1);
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #heading>Cerita Saya</template>
+    Lebih dari empat tahun mengimplementasi infrastruktur networking, storage
+    dan server. Selalu beradaptasi dan ingin tahu dengan teknologi baru maupun
+    teknologi yang belum pernah dipelajari sebelumnya.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #heading>Data Diri</template>
+    Mari berkoneksi dan mengenal saya lebih dekat dengan mengunjungi halaman
+    <a target="_blank" href="https://linkedin.com/in/dreiki">Linked In</a> saya.
+  </WelcomeItem>
+</template>
